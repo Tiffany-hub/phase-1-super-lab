97a29db94b9b3ceab610f362b1d320e60d120533
Tiffany Mwangi <tiffany.mwagi@student.moringaschool.com>
2023-07-12 15:10:52 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your classes here
+class Tree {
+    constructor(species) {
+      this.species = species;
+    }
+  
+    static definition() {
+      return 'A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.';
+    }
+  }
+  
+  class Deciduous extends Tree {
+    constructor(species, name) {
+      super(species);
+      this.name = name;
+    }
+  
+    static definition() {
+      return `${super.definition()} Deciduous trees shed their leaves annually.`;
+    }
+  }
+  
+  class Evergreen extends Tree {
+    constructor(species, name) {
+      super(species);
+      this.name = name;
+    }
+  
+    static definition() {
+      return `${super.definition()} Evergreens keep their leaves all year round.`;
+    }
+  }
+  const tree = new Tree ('Oak');
+  console.log(tree.species);
+  console.log(Tree.definition());
+
+  const deciduousTree = new Deciduous('Maple', 'Red Maple');
+  console.log(deciduousTree.species);
+  console.log(deciduousTree.name);
+  console.log(Deciduous.definition());
+
+  const evergreenTree = new Evergreen('Pine','Scots Pine');
+  console.log(evergreenTree.species);
+  console.log(evergreenTree.name);
+  console.log(Evergreen.definition);
